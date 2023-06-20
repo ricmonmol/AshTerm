583e7b05e5ddd6e3bc8257039b0033939a2ec68a
ricmonmol <jacksonfkk@gmail.com>
2023-06-20 18:31:55 -0300

    portfolio

--- src/components/Commands/commands.js
+++ src/components/Commands/commands.js
@@ -1,159 +1,102 @@
 /* eslint-disable import/no-anonymous-default-export */
-import getcat from "../../utils/cat"
-import getnp from "../../utils/spotify"
 export default {
-    commands: {
-        echo: {
-            description: 'Prints the given text to the console',
-            usage: 'echo <text>',
-            fn: (...args) => args.join(" ")
-        },
-        // cat: {
-        //     description: 'Get a cute cat image.',
-        //     usage: 'cat',
-        //     fn: async () => {
-        //         const url = await getcat()
-        //         window.open(url, '_blank')
-        //         return "fetching cat...\ncat fetched successfully!"
-        //     }
-        // },
-        about: {
-            description: 'About Me.',
-            usage: 'about',
-            fn: () => {
-                return "About Me.\n---\nmale\n18\nstudent\nsolodev\nmelophile\nweeb\ndumb\napathetic\n---\n"
-            }
-        },
-        twitter: {
-            description: 'Opens my Twitter Handle.',
-            usage: 'twitter',
-            fn: () => {
-                window.open('https://twitter.com/_asheeshh', '_blank')
-                return "opening twitter handle..."
-            }
-        },
-        github: {
-            description: 'Opens my GitHub Profile.',
-            usage: 'twitter',
-            fn: () => {
-                window.open('https://github.com/asrvd', '_blank')
-                return "opening github account..."
-            }
-        },
-        discord: {
-            description: 'Opens my Discord Account.',
-            usage: 'twitter',
-            fn: () => {
-                window.open('https://discordapp.com/users/784363251940458516', '_blank')
-                return "opening discord account..."
-            }
-        },
-        languages: {
-            description: 'Languages I know.',
-            usage: 'languages',
-            fn: () => {
-                return `
-                    these are the languages I know.\n---\n
-                    english          - 70%
-                    hindi            - 100%
-                    gen-z-langauge   - 00%
-                    python           - 60%
-                    javascript       - 60%
-                    html5            - 90%
-                    css3             - 80%
-                    ruby             - 40%
-                    rust             - 10% - [learning]
-                    go-lang          - 10% - [learning]\n---\n
-                `
-            }
-        },
-        skills: {
-            description: 'Skills I have.',
-            usage: 'skills',
-            fn: () => {
-                return `
-                    these are the skills I have.\n---\n
-                    procrastination  - 100%
-                    coding           - 50%
-                    studying         - 10%
-                    overthinking     - 100%
-                    social-skills    - 00%
-                    making-playlists - 100%\n---\n
-                `
-            }
-        },
-        projects: {
-            description: 'Projects I have worked on.',
-            usage: 'projects',
-            fn: () => {
-                return `
-                    Cool projects I have worked on.\n---\n
-                    'ashterm'                         | 'terminal portfolio'   | 'javascript'
-                    'octocolor'                       | 'github profile color' | 'javascript'
-                    'kanna-chan'                      | 'Discord-Bot'          | 'python'
-                    'wordinal'                        | 'wordle-on-terminal'   | 'javascript'
-                    'weeby.py'                        | 'API-wrapper'          | 'python'
-                    'gitbanner'                       | 'banner-generator'     | 'javascript'\n---\n
-                `
-            }
-        },
-        editor: {
-            description: 'Details about my current editor',
-            usage: 'editor',
-            fn: () => {
-                return `
-                    Editor: Visual Studio Code\n
-                    Theme : Catpuccin\n
-                    Font  : Consolas
-                `
-            }
-        },
-        repo: {
-            description: "Opens this website's github repository.",
-            usage: 'repo',
-            fn: () => {
-                window.open("https://github.com/asrvd/AshTerm", '_blank')
-                return "opening repository..."
-            }
-        },
-        spotify: {
-            description: 'Get info about my recently played song.',
-            usage: 'spotify',
-            fn: async () => {
-                const item = await getnp()
-                return `
-                    Now Playing/Recently Played\n
-                    ---\n
-                    Song: ${item.song}\n
-                    Artist: ${item.artist}\n---\n
-                `
-            }
-        }
+  commands: {
+    echo: {
+      description: "Prints the given text to the console",
+      usage: "echo <text>",
+      fn: (...args) => args.join(" "),
     },
-    overwrites:{
-        help: {
-            description: 'List all available commands',
-            usage: 'help',
-        },
-        cd: {
-            description: 'Change directory, not really, lol!',
-            usage: 'cd <directory>',
-        },
-        ls: {
-            description: 'List files in the current directory',
-            usage: 'ls',
-        },
-        mkdir: {
-            description: 'Make a directory',
-            usage: 'mkdir <directory>',
-        },
-        clear: {
-            description: 'Clears the terminal',
-            usage: 'clear'
-        },
-        cat: {
-            description: 'Get a cute cat image.',
-            usage: 'cat',
-        }
-    }
-}
+    about: {
+      description: "About Me.",
+      usage: "about",
+      fn: () => {
+        return `About Me.\n---\n
+        My name is Ricardo Montecinos, I live in Tandil, Argentina.
+        \nI'm a fullstack developer, I speak fluent english, I also like urban farming, 3D printing, archery, music, and a lot of other nerd stuff.\n---\n`;
+      },
+    },
+    twitter: {
+      description: "Opens my Twitter profile.",
+      usage: "twitter",
+      fn: () => {
+        window.open("https://twitter.com/ric_montecinos", "_blank");
+        return "opening twitter handle...";
+      },
+    },
+    github: {
+      description: "Opens my GitHub profile.",
+      usage: "github",
+      fn: () => {
+        window.open("https://github.com/ricmonmol", "_blank");
+        return "opening github account...";
+      },
+    },
+    linkedin: {
+      description: "Opens my LinkedIn profile.",
+      usage: "LinkedIn",
+      fn: () => {
+        window.open(
+          "https://www.linkedin.com/in/ricardo-montecinos-molina/",
+          "_blank"
+        );
+        return "opening linkedin account...";
+      },
+    },
+    languages: {
+      description: "Languages I know.",
+      usage: "languages",
+      fn: () => {
+        return `
+                    these are the languages and tools I know.\n---\n
+                    Javascript, Mongodb, Express, React, Node, Redux, Sequelize, HTML, CSS, Linux, Raspberry, Arduino.\n---\n
+                `;
+      },
+    },
+    projects: {
+      description: "Projects I have worked on.",
+      usage: "projects",
+      fn: () => {
+        return `
+                    Cool projects I have worked on. You can call them by thier names as commands\n---\n
+                    'cryptowatcher'   | 'monitor with CoinGecko API' - 'React'
+                    'countriesapp'    | 'An App about countries' - 'React-Node-Express-Sequelize'
+                    'urbanbuy'        | 'An App for ecommerce' - 'React-Node-Mongodb-Express'
+                `;
+      },
+    },
+    cryptowatcher: {
+      description: "Opens my project repo.",
+      usage: "github",
+      fn: () => {
+        window.open("https://github.com/ricmonmol/Crypto-App_react", "_blank");
+        return "opening github project...";
+      },
+    },
+    countriesapp: {
+      description: "Opens my project repo.",
+      usage: "github",
+      fn: () => {
+        window.open("https://github.com/ricmonmol/countries_henry", "_blank");
+        return "opening github project...";
+      },
+    },
+    urbanbuy: {
+      description: "Opens my project repo.",
+      usage: "github",
+      fn: () => {
+        window.open("https://github.com/jclima01/Proyecto-Final", "_blank");
+        return "opening github project...";
+      },
+    },
+  },
+  overwrites: {
+    help: {
+      description: "List all available commands",
+      usage: "help",
+    },
+    clear: {
+      description: "Clears the terminal",
+      usage: "clear",
+    },
+  },
+};
